3bdd30ef79f169d2434519462004dee7f770d9bf
StecLukasz <lukaszstec14@gmail.com>
2021-03-21 10:57:31 +0100

    Konfiguracja Entity

--- src/main/java/pl/sdacademy/database/entity/Run.java
+++ src/main/java/pl/sdacademy/database/entity/Run.java
@@ -1,5 +1,47 @@
 package pl.sdacademy.database.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "RUN")
 public class Run {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Integer membersLimit;
+
+    public Run() {
+    }
+
+    public Run(Long id, String name, Integer membersLimit) {
+        this.id = id;
+        this.name = name;
+        this.membersLimit = membersLimit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMembersLimit() {
+        return membersLimit;
+    }
+
+    public void setMembersLimit(Integer membersLimit) {
+        this.membersLimit = membersLimit;
+    }
 }
